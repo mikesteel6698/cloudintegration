b5a856d7fecfcc9a129f8870242635974875add5
mikesteel6698 <revanth6698@gmail.com>
2022-08-24 20:53:01 +0530

    new5

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  type    = string
+}
+
+variable "test_az" {
+  type    = string
+}
+
+variable "ami_id" {
+  type    = string
+}
+
+variable "instance_type" {
+  type    = string
+}
+
+variable "key" {
+  type = string
+}
+
+variable "root_volume_size" {
+  type    = number
+}
